d6e60bc1178fd129a34da8bba993cf4b13428493
KockataEPich <kostadin.belchev@abv.bg>
2022-10-06 23:13:08 +0200

    Clean animation files

--- MazingLabyrinthRun/code/include/animation/animation.h
+++ MazingLabyrinthRun/code/include/animation/animation.h
@@ -7,59 +7,13 @@
 
 #include "../resource/textureEnum.h"
 
-class EndAnimationException : public std::exception {
-public:
-	explicit EndAnimationException(std::string str) noexcept : m_message(std::move(str)) {}
-	const char* what() const noexcept override { return m_message.c_str(); }
-
-private:
-	std::string m_message;
-};
-
 using Repeat = struct { int numberOfTimes; };
 using AnimationSpeed = struct { float speed; };
-
 
 class Animation {
 public:
 	Animation(){};
 
-	Animation(const std::string& name, std::vector<Textures::ID>& frames, const Repeat& repeat, const AnimationSpeed& speed)
-	    : m_name{name}
-	    , m_frames{&frames}
-	    , m_repeatNumber{repeat.numberOfTimes}
-	    , m_speed{speed.speed} {};
-
-	Animation(const std::string& name, Textures::ID frames, const Repeat& repeat, const AnimationSpeed& speed)
-	: m_name{name}
-	, m_frame{frames}
-	, m_repeatNumber{repeat.numberOfTimes}
-	, m_speed{speed.speed} {};
-
-	void nextFrame();
-	void nextFrameWithStyle();
-	Textures::ID currentFrame() { return m_frames->at(m_frameIndex); }
-
-	void resetAnimation();
-	void resetAnimationWithStyle();
-	bool animationCycleFinish();
-
-	float speed() { return m_speed; }
-	bool operator==(const Animation animation);
-	void operator=(const Animation animation);
-
-protected:
-	std::string m_name;
-	int m_frameIndex = 0;
-	int m_timesPlayed = 0;
-	int m_repeatNumber;
-	float m_speed;
-	std::vector<Textures::ID>* m_frames;
-
-
-	Textures::ID m_frame;
-	sf::IntRect rectangleSource{0, 0, 16, 16};
-	int current_left_offset = 0;
 };
 
 #endif
--- MazingLabyrinthRun/code/include/animation/animationPlayer.h
+++ MazingLabyrinthRun/code/include/animation/animationPlayer.h
@@ -8,33 +8,4 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <unordered_map>
 
-class AnimationPlayer {
-public:
-	struct DefaultAnimations {
-		Animation facingRight;
-		Animation facingLeft;
-		Animation facingUp;
-		Animation facingDown;
-	};
-
-	AnimationPlayer(DefaultAnimations& defaultAnimations);
-
-	sf::Texture& getCurrentTexture();
-	void playAnimation(Animation& animation);
-	void playAnimationWithStyle(Animation& animation);
-	void update(const FacingSide& side, float deltaTime);
-	void playDefaultAnimation(const FacingSide& side);
-
-private:
-	float m_totalTime = 0.0f;
-	DefaultAnimations m_defaultAnimations;
-	bool with_style = false;
-	Animation m_currentAnimation;
-	sf::Texture* m_currentTexture;
-
-	void updateCurrentTexture() {
-		m_currentTexture = ResourceManager::getInstance()->getTexture(m_currentAnimation.currentFrame());
-	}
-};
-
 #endif
