73f0d35311dc1a1f15e6960a203ab28cc662f96d
KockataEPich <kostadin.belchev@abv.bg>
2022-10-28 14:58:01 +0200

    Refactored animation

--- MazingLabyrinthRun/code/include/animation/animation.h
+++ MazingLabyrinthRun/code/include/animation/animation.h
@@ -10,14 +10,10 @@
 #include <string>
 #include <vector>
 
-using Repeat = struct { int number_of_times; };
-using AnimationSpeed = struct { float speed; };
-
 class Animation {
 public:
 	Animation(const Textures::ID& texture_id)
-	    : m_repeat{Textures::texture_id_to_texture_metadata.at(m_texture_id).default_repeats}
-	    , m_speed{Textures::texture_id_to_texture_metadata.at(m_texture_id).default_speed}
+	    : m_speed{Textures::texture_id_to_texture_metadata.at(m_texture_id).default_speed}
 	    , m_texture_id{texture_id}
 	    , m_texture{ResourceManager::get_instance()->get_texture(texture_id)} {
 		m_x_axis_offset =
@@ -28,21 +24,13 @@ public:
 	bool next_frame();
 	bool reset_cycle();
 
-	float speed() { return m_speed.speed; }
+	float speed() { return m_speed; }
 
 	sf::IntRect& get_current_rect() { return m_current_rectange; }
 	sf::Texture* get_texture() { return m_texture; }
 	Textures::ID& get_texture_id() { return m_texture_id; }
 
-	void operator=(Animation& other) noexcept {
-		m_x_axis_offset = other.m_x_axis_offset;
-		m_texture_id = other.m_texture_id;
-		m_repeat = other.m_repeat;
-		m_speed = other.m_speed;
-		m_texture = other.m_texture;
-
-		m_current_rectange = sf::IntRect{0, 0, other.m_x_axis_offset, (int)m_texture->getSize().y};
-	}
+	void operator=(Animation& other) noexcept;
 	friend bool operator==(const Animation& lhs, const Animation& rhs);
 
 private:
@@ -51,8 +39,8 @@ private:
 
 	Textures::ID m_texture_id;
 	sf::Texture* m_texture;
-	Repeat m_repeat;
-	AnimationSpeed m_speed;
+	int m_number_of_cycles;
+	float m_speed;
 };
 
 #endif
--- MazingLabyrinthRun/code/src/animation/animation.cpp
+++ MazingLabyrinthRun/code/src/animation/animation.cpp
@@ -2,21 +2,22 @@
 
 bool Animation::next_frame() {
 	m_current_rectange.left += m_x_axis_offset;
-	if (m_current_rectange.left == m_texture->getSize().x) { return true; }
-
-	return false;
+	return m_current_rectange.left == m_texture->getSize().x;
 }
 
 bool Animation::reset_cycle() {
-	if (m_repeat.number_of_times == -1) {
-		m_current_rectange.left = 0;
-		return false;
-	}
-
-	if (--m_repeat.number_of_times == 0) return true;
-
+	if (m_number_of_cycles == 0) return true;
 	m_current_rectange.left = 0;
 	return false;
 }
 
 bool operator==(const Animation& lhs, const Animation& rhs) { return lhs.m_texture_id == rhs.m_texture_id; }
+
+void Animation::operator=(Animation& other) noexcept {
+	m_x_axis_offset = other.m_x_axis_offset;
+	m_texture_id = other.m_texture_id;
+	m_number_of_cycles = other.m_number_of_cycles;
+	m_speed = other.m_speed;
+	m_texture = other.m_texture;
+	m_current_rectange = sf::IntRect{0, 0, other.m_x_axis_offset, (int)m_texture->getSize().y};
+}
