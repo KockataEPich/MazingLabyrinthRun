285217f1e2672fef34108ed14274680785011b96
KockataEPich <kostadin.belchev@abv.bg>
2022-11-01 00:51:06 +0100

    Sprites now sorted before rendered

--- MazingLabyrinthRun/code/src/system/systems/render_system.cpp
+++ MazingLabyrinthRun/code/src/system/systems/render_system.cpp
@@ -1,6 +1,22 @@
 #include "../include/system/systems/render_system.h"
 
 void Render::render() {
+
+	std::sort(std::begin(m_registered_entities),
+	          std::end(m_registered_entities),
+	          [this](const auto& lhs, const auto& rhs) {
+		          ComponentHandle<SpriteComponent> lhs_sprite;
+		          ComponentHandle<SpriteComponent> rhs_sprite;
+
+		          m_parent_world->unpack(lhs, lhs_sprite);
+		          m_parent_world->unpack(rhs, rhs_sprite);
+
+		          auto lhs_pos = lhs_sprite->m_sprite.getPosition().y;
+		          auto rhs_pos = rhs_sprite->m_sprite.getPosition().y;
+
+		          return  lhs_pos < rhs_pos;
+	          });
+
 	for (auto& entity : m_registered_entities) {
 		ComponentHandle<SpriteComponent> sprite;
 		m_parent_world->unpack(entity, sprite);
